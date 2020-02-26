bb4d83d44b8fed8f180ecb0c98600775768645f6
Donghyun <markk628@gmail.com>
2020-02-25 22:12:25 -0800

    templates for pmanagere app works

--- pmanager/admin.py
+++ pmanager/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from pmanager.models import Platform
 
 # Register your models here.
+class PlatformAdmin(admin.ModelAdmin):
+    list_display = ('platform', 'username', 'password')
+
+admin.site.register(Platform, PlatformAdmin)
