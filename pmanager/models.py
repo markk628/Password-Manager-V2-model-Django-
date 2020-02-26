3b5011e41dc56219e092a91eeb2e38f0998c4eb4
Donghyun <markk628@gmail.com>
2020-02-25 20:58:47 -0800

    created platform detail class in pmanager views

--- pmanager/models.py
+++ pmanager/models.py
@@ -6,7 +6,21 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Password(models.Model):
+class Platform(models.Model):
     platform = models.CharField(max_length=20)
     username = models.CharField(max_length=settings.PMANAGER_USERNAME_MAX_LENGTH)
     password = models.CharField(max_length=30)
+    slug = models.CharField(max_length=settings.PMANAGER_USERNAME_MAX_LENGTH, blank=True, editable=False)
+
+    def __str__(self):
+        return self.platform
+
+    def get_url(self):
+        path_components = {'slug': self.slug}
+        return reverse('pm-platform-page', kwargs=path_components)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.slug = slugify(self.platform, allow_unicode=True)
+
+        return super(Platform, self).save(*args, **kwargs)
--- pmanager/views.py
+++ pmanager/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+
+from pmanager.models import Platform
 
 # Create your views here.
+class PlatformDetailView(DetailView):
+    model = Platform
+
+    def get(self, request, slug):
+        platform = self.get_queryset().get(slug_iexact=slug)
+        return render(request, 'platform.html', {
+            'platform': platform
+        })
