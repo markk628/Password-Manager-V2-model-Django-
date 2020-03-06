ed66cd6de31ff4f3a638c70832b044816a866619
Donghyun <markk628@gmail.com>
2020-03-06 09:29:13 -0800

    update route works

--- pmanager/urls.py
+++ pmanager/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', PlatformListView.as_view(), name='platform-list-page'),
     path('<int:pk>/', PlatformDetailView.as_view(), name='platform-detail-page'),
     path('create/', CreateNewPlatform.as_view(), name='platform-create-page'),
-    path('(?P<pk>[0-9]+)/$/update/', UpdatePlatform.as_view(), name='platform-update-page'),
+    path('<int:pk>/update/', UpdatePlatform.as_view(), name='platform-update-page'),
     path('<int:pk>/delete', deletePlatform, name='platform-delete'),
 ]
